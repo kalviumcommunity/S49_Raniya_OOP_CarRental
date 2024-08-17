f1a3b56a443cb8b5aadf51dddde51121a8c4aa1e
raniyaptla <ra.niyapoonthala@gmail.com>
2024-08-17 10:26:57 +0530

    updt

--- user.cpp
+++ user.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
 #include <string>
 
-using namesapce std;
+using namespace std;
 
 //user class
 class User{
     public:
         int userID;
         string name;
-}
+
+    //constructor
+    User(int userID,string name){
+        this->userID=userID;
+        this->name=name;
+    }    
+
+
+    // Method to register the user
+    void registerUser() {
+        cout << "User " << name << " registered." << endl;
+    }
+
+    // Method to login the user
+    void login() {
+        cout << "User " << name << " logged in." << endl;
+    }
+
+};
