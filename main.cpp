8d5929797b30c5101513070208f6bc9f50972e2a
raniyaptla <raniyapoonthala@gmail.com>
2024-10-29 10:29:30 +0530

    Milestone-4 | Abstract class and Virtual Function_new

--- main.cpp
+++ main.cpp
@@ -1,28 +1,29 @@
+
+
 #include <iostream>
 #include <string>
 #include "car.cpp"
 #include "user.cpp"
 
 
-int main() {
-    // Create an ElectricCar object
+int main() {  
+
+
+// Create an ElectricCar object
     ElectricCar eCar(101, "Tesla Model 3", true, 120.0, 75.0);
     
     // Create a HybridCar object
     HybridCar hCar(202, "Toyota Prius", true, 100.0, 60.0, 50.0);
 
-    // Polymorphism following the Liskov Substitution Principle (LSP)
-    Car* carPtr;
-
-    // LSP: Substituting base class pointer with ElectricCar instance
-    carPtr = &eCar;
+    
     cout << "Electric Car details:" << endl;
-    carPtr->displayDetails();  // Calls ElectricCar's displayDetails method 
+    eCar.displayDetails();  // Calls ElectricCar's displayDetails
 
-    // LSP: Substituting base class pointer with HybridCar instance
-    carPtr = &hCar;
+    
     cout << "\nHybrid Car details:" << endl;
-    carPtr->displayDetails();  // Calls HybridCar's displayDetails method 
+    hCar.displayDetails();  // Calls HybridCar's displayDetails
 
+   
     return 0;
 }
+
